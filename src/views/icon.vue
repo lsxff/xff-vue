f5d2c3c1b37e82e40f2a68ea07b5ab1e6ac36dec
liangshixu <xinnuotec@163.com>
2021-10-09 23:22:22 +0800

    Icon组件

---
+++ src/views/icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <xxf-icon icon="icon-check" color="red" size="24"></xxf-icon>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
